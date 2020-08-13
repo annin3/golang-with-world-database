5046a4a8fca600633f8e00ed0b771d8faf619606
annin3 <shunta.titech@gmail.com>
2020-08-13 22:00:15 +0900

    tokyo

--- go.mod
+++ go.mod
@@ -5,4 +5,12 @@ go 1.14
 require (
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/jmoiron/sqlx v1.2.0
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/echo/v4 v4.1.16 // indirect
+	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/mattn/go-colorable v0.1.6 // indirect
+	github.com/valyala/fasttemplate v1.1.0 // indirect
+	golang.org/x/crypto v0.0.0-20200221231518-2aa609cf4a9d // indirect
+	golang.org/x/net v0.0.0-20200226121028-0de0cce0169b // indirect
+	golang.org/x/text v0.3.2 // indirect
 )
--- main.go
+++ main.go
@@ -3,28 +3,49 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
+
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
+	"github.com/labstack/echo"
 )
 
 type City struct {
-	ID          int    `json:"id,omitempty"	db:"ID"`
-	Name        string `json:"name,omitempty"	db:"Name"`
-	CountryCode string `json:"countryCode,omitempty"	db:"CountryCode"`
-	District    string `json:"district,omitempty"	db:"District"`
-	Population  int    `json:"population,omitempty	db:"Population`
+	ID          int    `json:"id,omitempty"  db:"ID"`
+	Name        string `json:"name,omitempty"  db:"Name"`
+	CountryCode string `json:"countryCode,omitempty"  db:"CountryCode"`
+	District    string `json:"district,omitempty"  db:"District"`
+	Population  int    `json:"population,omitempty"  db:"Population"`
 }
 
+var (
+	db *sqlx.DB
+)
+
 func main() {
-	db, err := sqlx.Connect("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOSTNAME"), os.Getenv("DB_PORT"), os.Getenv("DB_DATABASE")))
+	_db, err := sqlx.Connect("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOSTNAME"), os.Getenv("DB_PORT"), os.Getenv("DB_DATABASE")))
 	if err != nil {
 		log.Fatalf("Cannot Connect to Database: %s", err)
 	}
+	db = _db
 
-	fmt.Println("Connected!")
+	e := echo.New()
+
+	e.GET("/cities/:cityName", getCityInfoHandler)
+
+	e.Start(":11800")
+}
+
+func getCityInfoHandler(c echo.Context) error {
+	cityName := c.Param("cityName")
+	fmt.Println(cityName)
+
 	city := City{}
-	db.Get(&city, "SELECT * FROM city WHERE Name = 'Tokyo")
+	db.Get(&city, "SELECT * FROM city WHERE Name=?", cityName)
+	if city.Name == "" {
+		return c.NoContent(http.StatusNotFound)
+	}
 
-	fmt.Printf("Tokyoの人口は%d人です\n", city.Population)
+	return c.JSON(http.StatusOK, city)
 }
